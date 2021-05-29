8e205e1342e052d1338c888484ccc2e0b05f0354
J. Andres Gutierrez <workjorge@yahoo.com>
2021-05-29 17:33:10 -0500

    testing GUI

--- src/gui.py
+++ src/gui.py
@@ -1,1 +1,14 @@
 from tkinter import *
+
+root = Tk()
+
+# hexadecimal colour box
+hexbox = Entry(root, width=20, bg="white", fg="black")
+hexbox.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+hexbox.insert(0,"#00000")
+
+hexbox.pack()
+
+
+root.mainloop()
+
