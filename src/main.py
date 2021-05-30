041a0d490f62eff68f9664510c123dc33009a1a7
J. Andres Gutierrez <workjorge@yahoo.com>
2021-05-29 19:09:19 -0500

    setup for development of GUI

--- src/main.py
+++ src/main.py
@@ -1,13 +1,15 @@
 from tkinter import *
+from wheel import wheel as guiWheel
+
 
 root = Tk()
+root.title("swc 0.01b")
 
 # hexadecimal colour box
-hexbox = Entry(root, width=20, bg="white", fg="black")
+hexbox = Entry(root, width=8, bg="white", fg="black")
 hexbox.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 hexbox.insert(0,"#00000")
 
-hexbox.pack()
 
 
 root.mainloop()
---
+++ src/wheel.py
@@ -0,0 +1,21 @@
+# colorwheel widget
+class wheel:
+
+    def __init__(self, e):
+        self.hex_value = 0x0
+        self.element = e
+        self.radius = 100
+        self.gui_hex = [
+          [0xff, 0x00, 0xff], 
+          [0xff, 0x00, 0x00], 
+          [0xff, 0xff, 0x00], 
+          [0x00, 0xff, 0x00], 
+          [0x00, 0xff, 0xff], 
+          [0x00, 0x00, 0xff], 
+          [0xff, 0x00, 0xff]]
+
+        def update_hexvalue():
+            pass
+
+        def draw_wheel():
+            pass
